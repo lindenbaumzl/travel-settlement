a0923de44c225c874628c5e4e73d2192d1f73633
lindenbaumzl <lindenbaum.zl@gmail.com>
2026-04-25 11:23:29 +0900

    Delete storage.js

--- storage.js
+++
@@ -1,1 +0,0 @@
-import{KEY}from'./config.js';export const load=()=>{try{return JSON.parse(localStorage.getItem(KEY)||'[]')}catch{return[]}};export const save=trips=>localStorage.setItem(KEY,JSON.stringify(trips));
